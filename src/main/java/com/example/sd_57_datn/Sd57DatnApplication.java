ecab2e4c2aff5c709a5d57aa14702365a4ea43b1
Vinh2712 <vinhnqph28348@fpt.edu.vn>
2024-05-24 22:20:00 +0700

    model

--- src/main/java/com/example/sd_57_datn/Sd57DatnApplication.java
+++ src/main/java/com/example/sd_57_datn/Sd57DatnApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Sd57DatnApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Sd57DatnApplication.class, args);
-    }
+    public static void main(String[] args) { SpringApplication.run(Sd57DatnApplication.class, args); }
 
 }
