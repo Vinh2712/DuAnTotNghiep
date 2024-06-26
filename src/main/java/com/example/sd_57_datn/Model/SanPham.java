cf1c001073cb0184e8d5e81101b52b05b3810533
NgoocKhanh22 <118629037+Khanhtnph27758@users.noreply.github.com>
2024-06-26 21:44:53 +0700

    Code 26_6

--- src/main/java/com/example/sd_57_datn/Model/SanPham.java
+++
@@ -1,7 +0,0 @@
-package com.example.sd_57_datn.Model;
-public class SanPham {
-    public static void main(String[] args) {
-//        Le Anh Nhat
-//        AHIHI
-    }
-}
--- src/main/java/com/example/sd_57_datn/Service/GiayTheThao/GiayTheThaoChiTietImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.sd_57_datn.Service.GiayTheThao;
-
-import com.example.sd_57_datn.Model.GiayTheThaoChiTiet;
-
-import java.util.UUID;
-
-public interface GiayTheThaoChiTietImpl {
-    GiayTheThaoChiTiet findIdByIdGiayTheThao(UUID id, UUID idMauSac, UUID idSize);
-}
